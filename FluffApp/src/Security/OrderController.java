27a1642e396ccd3e0327c9b1ad409d8c47585baf
Tara_Donald <taralee.kd.db@gmail.com>
2025-03-13 00:38:40 -0500

    CreateOrder logic being updated

---
+++ FluffApp/src/Security/OrderController.java
@@ -0,0 +1,15 @@
+package Security;
+
+import java.util.ArrayList;
+
+public class OrderController{
+    public ArrayList<Order> loadOrders(){
+
+    }
+
+    public void addOrder(){
+
+    }
+
+    public void deleteOrder(){}
+}

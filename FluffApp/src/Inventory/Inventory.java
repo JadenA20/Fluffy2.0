7f0735307713c632bfd6fb896a0b52fb4f0cd052
khanezwal464 <khanezwallace18@gmail.com>
2025-03-25 23:53:37 -0500

    Update Inventory.java

--- FluffApp/src/Inventory/Inventory.java
+++ FluffApp/src/Inventory/Inventory.java
@@ -1,5 +1,33 @@
 package Inventory;
 
 public class Inventory {
+//Inventory
+private int id,stockCount,priorityStatus;
+private static int count =1;
+private String name, item_desc;
+
+public Inventory(int id, int stockCount, int priorityStatus,String name, String item_desc){
+    this.id=count;
+    this.stockCount = stockCount;
+    this.priorityStatus = priorityStatus;
+    this.name = name;
+    this.item_desc = item_desc;
+    count++;
+}
+
+public void setStockCount(int newStockCount){
+    this.stockCount = newStockCount;
+}
+
+public void setPriorityStatus(int newPriority){
+    this.priorityStatus = newPriority;
+}
+
+public void setName(String newName){
+    this.name = newName;
+}
+public void setDescription(String newDescrip){
+    this.item_desc = newDescrip;
+}
 
 }
